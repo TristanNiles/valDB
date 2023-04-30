e3860d186e0c42d09dcf05a01b974e9431e9aa4b
Tristan <t070m625@ku.edu>
2023-04-30 12:33:21 -0500

    style sortable table and scrollbar

--- sortable.php
+++ sortable.php
@@ -7,7 +7,22 @@ if ($qry == false) {
     $all = $qry->fetch_all();
     $height = count($all);
     $width = count($all[0]);
-    echo "<table>";
+    echo "<table class='sortable'>";
+    echo "<tr>
+            <th>Players</th>
+            <th>GPU</th>
+            <th>Mouse</th>
+            <th>Keyboard</th>
+            <th>Crosshair</th>
+            <th>Sensitivity</th>
+            <th>First bloods</th>
+            <th>First deaths</th>
+            <th>ACS</th>
+            <th>KD</th>
+            <th>Win percentage</th>
+            <th>HS</th>
+            <th>KAST</th>
+        </tr>";
     for ($i = 0; $i < $height; $i++){
         echo "<tr>";
         for ($j = 0; $j < $width; $j++) {
